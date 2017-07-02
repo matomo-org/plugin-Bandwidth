8587186cee330d7ec208339d1ddc77525ae403d3
sgiehl <stefan@piwik.org>
2017-07-02 20:24:31 +0200

    Show bandwidth as action detail

---
+++ VisitorDetails.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link    http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\Bandwidth;
+
+use Piwik\Metrics\Formatter;
+use Piwik\Plugins\Live\VisitorDetailsAbstract;
+use Piwik\View;
+
+class VisitorDetails extends VisitorDetailsAbstract
+{
+    public function extendActionDetails(&$action, $nextAction, $visitorDetails)
+    {
+        if ($action['bandwidth']) {
+            $formatter = new Formatter();
+            $action['bandwidth_pretty'] = $formatter->getPrettySizeFromBytes($action['bandwidth']);
+        }
+    }
+
+    public function renderActionTooltip($action, $visitInfo)
+    {
+        if ($action['bandwidth']) {
+            $view = new View('@Bandwidth/_actionTooltip');
+            $view->action = $action;
+            return $view->render();
+        }
+
+        return '';
+    }
+}
