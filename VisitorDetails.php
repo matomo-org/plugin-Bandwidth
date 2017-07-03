c53ae62dc7c652745f2e0860816c3e88730ef447
sgiehl <stefan@piwik.org>
2017-07-03 16:02:13 +0200

    Show bandwidth statistics in visitor profile

--- VisitorDetails.php
+++ VisitorDetails.php
@@ -17,7 +17,7 @@ class VisitorDetails extends VisitorDetailsAbstract
     public function extendActionDetails(&$action, $nextAction, $visitorDetails)
     {
         if (array_key_exists('bandwidth', $action)) {
-            $formatter = new Formatter();
+            $formatter                  = new Formatter();
             $action['bandwidth_pretty'] = $formatter->getPrettySizeFromBytes($action['bandwidth']);
         }
     }
@@ -25,11 +25,60 @@ class VisitorDetails extends VisitorDetailsAbstract
     public function renderActionTooltip($action, $visitInfo)
     {
         if (!empty($action['bandwidth'])) {
-            $view = new View('@Bandwidth/_actionTooltip');
+            $view         = new View('@Bandwidth/_actionTooltip');
             $view->action = $action;
             return $view->render();
         }
 
         return '';
+    }
+
+    protected $sumBandwidth         = 0;
+    protected $actionsWithBandwidth = 0;
+    protected $maxBandwidth         = null;
+    protected $minBandwidth         = null;
+
+    public function initProfile($visits, &$profile)
+    {
+        $this->sumBandwidth         = 0;
+        $this->actionsWithBandwidth = 0;
+    }
+
+    public function handleProfileAction($action, &$profile)
+    {
+        if (!empty($action['bandwidth'])) {
+            $this->sumBandwidth += $action['bandwidth'];
+            if (is_null($this->minBandwidth)) {
+                $this->minBandwidth = $action['bandwidth'];
+            }
+            $this->minBandwidth = min($this->minBandwidth, $action['bandwidth']);
+            if (is_null($this->minBandwidth)) {
+                $this->minBandwidth = $action['bandwidth'];
+            }
+            $this->maxBandwidth = max($this->maxBandwidth, $action['bandwidth']);
+            $this->actionsWithBandwidth++;
+        }
+    }
+
+    public function finalizeProfile($visits, &$profile)
+    {
+        if ($this->actionsWithBandwidth) {
+            $formatter               = new Formatter();
+            $profile['sumBandwidth'] = $formatter->getPrettySizeFromBytes($this->sumBandwidth);
+            $profile['minBandwidth'] = $formatter->getPrettySizeFromBytes($this->minBandwidth);
+            $profile['maxBandwidth'] = $formatter->getPrettySizeFromBytes($this->maxBandwidth);
+            $profile['avgBandwidth'] = $formatter->getPrettySizeFromBytes(round($this->sumBandwidth / $this->actionsWithBandwidth));
+        }
+    }
+
+    public function renderProfileSummary($profile)
+    {
+        if (!isset($profile['avgBandwidth'])) {
+            return array();
+        }
+
+        $view              = new View('@Bandwidth/_profileSummary.twig');
+        $view->visitorData = $profile;
+        return array(array(5, $view->render()));
     }
 }
