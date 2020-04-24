235c37ea05bd8d34c78fbbd74eb502aa2fe8533d
sgiehl <stefan@matomo.org>
2020-04-24 10:00:36 +0200

    update doc blocks with new name

--- Archiver.php
+++ Archiver.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Piwik - free/libre analytics platform
+ * Matomo - free/libre analytics platform
  *
- * @link http://piwik.org
+ * @link https://matomo.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  *
  */
@@ -13,7 +13,6 @@ use Piwik\Tracker\Action;
 
 /**
  * Class Archiver
- * @package Piwik\Plugins\Bandwidth
  *
  * Archiver is class processing raw data into ready ro read reports.
  * It must implement two methods for aggregating daily reports
