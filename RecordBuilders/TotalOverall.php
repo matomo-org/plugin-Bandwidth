89967bfd90ab953ec0c18d07a128e8d255630857
diosmosis <diosmosis@users.noreply.github.com>
2023-05-19 19:54:27 -0700

    move archiving code to RecordBuilders

---
+++ RecordBuilders/TotalOverall.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+namespace Piwik\Plugins\Bandwidth\RecordBuilders;
+
+use Piwik\ArchiveProcessor;
+use Piwik\Plugins\Bandwidth\Archiver;
+
+class TotalOverall extends Base
+{
+    public function getRecordMetadata(ArchiveProcessor $archiveProcessor): array
+    {
+        return [
+            ArchiveProcessor\Record::make(ArchiveProcessor\Record::TYPE_NUMERIC, Archiver::BANDWIDTH_TOTAL_RECORD),
+        ];
+    }
+
+    protected function aggregate(ArchiveProcessor $archiveProcessor): array
+    {
+        $query = $this->queryActionsByDimension($archiveProcessor->getLogAggregator());
+        $record = $this->sumNumericRecord($query, 'sum_bandwidth');
+        return [Archiver::BANDWIDTH_TOTAL_RECORD => $record];
+    }
+}
---
+++ RecordBuilders/TotalPage.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+namespace Piwik\Plugins\Bandwidth\RecordBuilders;
+
+use Piwik\ArchiveProcessor;
+use Piwik\Plugins\Bandwidth\Archiver;
+use Piwik\Tracker\Action;
+
+class TotalPage extends Base
+{
+    public function getRecordMetadata(ArchiveProcessor $archiveProcessor): array
+    {
+        return [
+            ArchiveProcessor\Record::make(ArchiveProcessor\Record::TYPE_NUMERIC, Archiver::BANDWIDTH_PAGEVIEW_RECORD),
+        ];
+    }
+
+    protected function aggregate(ArchiveProcessor $archiveProcessor): array
+    {
+        $joinLogActionOnColumn = ['idaction_url'];
+        $query = $this->queryActionsByDimension($archiveProcessor->getLogAggregator(),
+            'log_action1.type = ' . Action::TYPE_PAGE_URL, $joinLogActionOnColumn);
+        $record = $this->sumNumericRecord($query, 'sum_bandwidth');
+        return [Archiver::BANDWIDTH_PAGEVIEW_RECORD => $record];
+    }
+}
