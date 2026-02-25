2f139ee9c686d8c31d4b9522639e412d108de346
Lachlan Reynolds <lachlan.reynolds@innocraft.com>
2026-02-26 11:30:43 +1300

    Added API documentation for Bandwidth

--- API.php
+++ API.php
@@ -15,10 +15,29 @@ use Piwik\DataTable;
 use Piwik\Piwik;
 
 /**
+ * Bandwidth API lets you request bandwidth metrics for page views and downloads.
+ *
  * @method static \Piwik\Plugins\Bandwidth\API getInstance()
  */
 class API extends \Piwik\Plugin\API
 {
+    /**
+     * Returns bandwidth metrics for the requested site, period, and segment.
+     *
+     * @param int $idSite The numeric ID of the website to query.
+     * @param string $period The period to process, processes data for the period containing the specified date.
+     *                       Allowed values: "day", "week", "month", "year", "range".
+     * @param string $date The date or date range to process.
+     *                     'YYYY-MM-DD', magic keywords (today, yesterday, lastWeek, lastMonth, lastYear),
+     *                     or date range (ie, 'YYYY-MM-DD,YYYY-MM-DD', lastX, previousX).
+     * @param string|false $segment (Optional) Custom segment to filter the report.
+     *                              Example: "referrerName==twitter.com"
+     *                              Supports AND (;) and OR (,) operators.
+     *                              [See documentation:](https://developer.matomo.org/api-reference/reporting-api-segmentation)
+     * @param string|array|false $columns Optional metric columns to return. Accepts a comma-separated list,
+     *                                    an array of metric names, or false to return all available metrics.
+     * @return DataTable|DataTable\Map Bandwidth metrics with integer values.
+     */
     public function get($idSite, $period, $date, $segment = false, $columns = false)
     {
         Piwik::checkUserHasViewAccess($idSite);
